1d64f00593546674784d9df4d9a3cf1c38df9681
pyxx2 <873037065@qq.com>
2024-10-28 17:37:17 +0800

    将数据库部署到AWS的rds上

--- src/com/coffee/db/DBConnection.java
+++ src/com/coffee/db/DBConnection.java
@@ -5,10 +5,10 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBConnection {
-    private static final String NAME = "root";
-    private static final String URL = "jdbc:mysql://localhost:3306/coffee?serverTimezone=UTC";
-    private static final String PASSWORD = "root";
-    private static final String DRIVER = "com.mysql.jdbc.Driver";
+    private static final String NAME = "main";  // RDS 用户名
+    private static final String URL = "jdbc:mysql://coffee3.csp7eqfx8y3e.us-east-1.rds.amazonaws.com:3306/coffee?serverTimezone=UTC";  // RDS 数据库 URL
+    private static final String PASSWORD = "lab_password";  // RDS 密码
+    private static final String DRIVER = "com.mysql.jdbc.Driver";  // 新版驱动
     private Connection conn = null;
 
     public Connection getCon() {
@@ -22,6 +22,5 @@ public class DBConnection {
         } catch (ClassNotFoundException | SQLException e) {
             throw new RuntimeException(e);
         }
-
     }
 }
